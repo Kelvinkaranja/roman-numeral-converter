e4fbe98c70238b40f055ec251510e08c0395aecb
Kelvinkaranja <116677736+Kelvinkaranja@users.noreply.github.com>
2022-11-15 15:18:36 +0300

    Update roman_to_arabic_converter.py

--- roman_to_arabic_converter.py
+++ roman_to_arabic_converter.py
@@ -5,7 +5,7 @@ import re
 roman = {'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90,
          'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1}
 
-
+#create a function that asks for input
 def string():
     string = input('ENTER A ROMAN NUMERAL: ')
     return string.upper()
